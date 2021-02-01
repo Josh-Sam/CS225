ba5e5e7fab979dd4a6494ac312843df7a8388933
Josh Samuel <joshps2@illinois.edu>
2021-01-31 21:19:02 -0600

    REPLACE THIS WITH YOUR COMMIT MESSAGE

--- lab_intro/cs225/HSLAPixel.cpp
+++ lab_intro/cs225/HSLAPixel.cpp
@@ -12,4 +12,23 @@
 using namespace std;
 
 namespace cs225 {
+  HSLAPixel::HSLAPixel(){
+    h = 360;
+    s = 1;
+    l = 1;
+    a = 1;
+  }
+  HSLAPixel::HSLAPixel(double hue, double saturation, double luminance){
+    h = hue;
+    s = saturation;
+    l = luminance;
+    a = 1;
+  }
+  HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha){
+    h = hue;
+    s = saturation;
+    l = luminance;
+    a = alpha;
+  }
+
 }
--- lab_intro/cs225/HSLAPixel.h
+++ lab_intro/cs225/HSLAPixel.h
@@ -11,4 +11,16 @@
 #include <sstream>
 
 namespace cs225 {
+  class HSLAPixel{
+    public:
+      double h, s, l, a;
+
+      HSLAPixel();
+      HSLAPixel(double hue, double saturation, double luminance);
+      HSLAPixel(double hue, double saturation, double luminance, double alpha);
+
+
+
+  };
+
 }
