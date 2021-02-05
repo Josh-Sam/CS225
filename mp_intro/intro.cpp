ae555be2d13ea3873933371d718050eb204e0dd6
Josh Samuel <joshps2@illinois.edu>
2021-02-05 01:47:20 -0600

    Completed MP

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -3,14 +3,32 @@
 
 #include <string>
 
+using cs225::HSLAPixel;
+using cs225::PNG;
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  PNG image, image2;
+  image.readFromFile(inputFile);
+  image2.readFromFile(inputFile);
+  for (unsigned x = 0; x < image.width(); x++) {
+    for (unsigned y = 0; y < image.height(); y++) {
+      HSLAPixel & pixel = image.getPixel(x, y);
+      image2.getPixel((image.width()-1) - x,(image.height()-1) - y) = pixel;
+    }
+  }
+  image2.writeToFile(outputFile);
+
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
-  // TODO: Part 3
+  for (unsigned x = 0; x < png.width(); x++) {
+    for (unsigned y = 0; y < png.height(); y++) {
+      HSLAPixel & pixel = png.getPixel(x, y);
+      pixel.h = (double(x+y)/(png.width() + png.height())) * 360;
+      pixel.l = 0.5;
+    }
+  }
 
   return png;
 }
