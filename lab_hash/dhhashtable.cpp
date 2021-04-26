94a98eec32751c8b350e8db223d9a81cd9428833
joshps2 <joshps2@illinois.edu>
2021-04-25 23:57:28 -0500

    fixed dh

--- lab_hash/dhhashtable.cpp
+++ lab_hash/dhhashtable.cpp
@@ -1,9 +1,9 @@
-/**
- * @file dhhashtable.cpp
- * Implementation of the DHHashTable class.
- */
-
 #include "dhhashtable.h"
+
+using hashes::hash;
+using hashes::secondary_hash;
+using std::pair;
+
 
 template <class K, class V>
 DHHashTable<K, V>::DHHashTable(size_t tsize)
@@ -11,7 +11,7 @@ DHHashTable<K, V>::DHHashTable(size_t tsize)
     if (tsize <= 0)
         tsize = 17;
     size = findPrime(tsize);
-    table = new std::pair<K, V>*[size];
+    table = new pair<K, V>*[size];
     should_probe = new bool[size];
     for (size_t i = 0; i < size; i++) {
         table[i] = NULL;
@@ -56,14 +56,14 @@ void DHHashTable<K, V>::destroy()
 template <class K, class V>
 void DHHashTable<K, V>::copy(const DHHashTable<K, V>& other)
 {
-    table = new std::pair<K, V>*[other.size];
+    table = new pair<K, V>*[other.size];
     should_probe = new bool[other.size];
     for (size_t i = 0; i < other.size; i++) {
         should_probe[i] = other.should_probe[i];
         if (other.table[i] == NULL)
             table[i] = NULL;
         else
-            table[i] = new std::pair<K, V>(*(other.table[i]));
+            table[i] = new pair<K, V>(*(other.table[i]));
     }
     size = other.size;
     elems = other.elems;
@@ -72,33 +72,46 @@ void DHHashTable<K, V>::copy(const DHHashTable<K, V>& other)
 template <class K, class V>
 void DHHashTable<K, V>::insert(K const& key, V const& value)
 {
-
-    /**
-     * @todo Implement this function.
-     *
-     * @note Remember to resize the table when necessary (load factor >=
-     *  0.7). **Do this check *after* increasing elems!!** Also, don't
-     *  forget to mark the cell for probing with should_probe!
-     */
-
-    (void) key;   // prevent warnings... When you implement this function, remove this line.
-    (void) value; // prevent warnings... When you implement this function, remove this line.
+    unsigned int cur_hash = hash(key, size);
+    unsigned int jump = secondary_hash(key, size);
+    while (table[cur_hash] != NULL) {
+        cur_hash = (cur_hash + jump) % size;
+    }
+    table[cur_hash] = new pair<K, V>(key, value);
+    should_probe[cur_hash] = true;
+    elems++;
+    if (shouldResize()) resizeTable();
 }
 
 template <class K, class V>
 void DHHashTable<K, V>::remove(K const& key)
 {
-    /**
-     * @todo Implement this function
-     */
+    unsigned int cur_hash = hash(key, size);
+    unsigned int jump = secondary_hash(key, size);
+    while (table[cur_hash] != NULL && table[cur_hash]->first != key) {
+        cur_hash = (cur_hash + jump) % size;
+    }
+    if (table[cur_hash] == NULL) return;
+    else if (table[cur_hash]->first == key) {
+        delete table[cur_hash];
+        table[cur_hash] = NULL;
+        elems--;
+    }
 }
 
 template <class K, class V>
 int DHHashTable<K, V>::findIndex(const K& key) const
 {
-    /**
-     * @todo Implement this function
-     */
+    unsigned int cur_hash = hash(key, size);
+    unsigned int jump = secondary_hash(key, size);
+    unsigned int start = cur_hash;
+    while (should_probe[cur_hash]) {
+        if (table[cur_hash] != NULL) {
+            if (table[cur_hash]->first == key) return cur_hash;
+        }
+        cur_hash = (cur_hash + jump) % size;
+        if (start == cur_hash) break;
+    }
     return -1;
 }
 
@@ -135,7 +148,7 @@ void DHHashTable<K, V>::clear()
 {
     destroy();
 
-    table = new std::pair<K, V>*[17];
+    table = new pair<K, V>*[17];
     should_probe = new bool[17];
     for (size_t i = 0; i < 17; i++)
         should_probe[i] = false;
@@ -147,7 +160,7 @@ template <class K, class V>
 void DHHashTable<K, V>::resizeTable()
 {
     size_t newSize = findPrime(size * 2);
-    std::pair<K, V>** temp = new std::pair<K, V>*[newSize];
+    pair<K, V>** temp = new pair<K, V>*[newSize];
     delete[] should_probe;
     should_probe = new bool[newSize];
     for (size_t i = 0; i < newSize; i++) {
@@ -157,8 +170,8 @@ void DHHashTable<K, V>::resizeTable()
 
     for (size_t slot = 0; slot < size; slot++) {
         if (table[slot] != NULL) {
-            size_t h = hashes::hash(table[slot]->first, newSize);
-            size_t jump = hashes::secondary_hash(table[slot]->first, newSize);
+            size_t h = hash(table[slot]->first, newSize);
+            size_t jump = secondary_hash(table[slot]->first, newSize);
             size_t i = 0;
             size_t idx = h;
             while (temp[idx] != NULL)
